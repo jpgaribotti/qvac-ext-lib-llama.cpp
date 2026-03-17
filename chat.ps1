f393e941bdca711263b8b4840f16f655c38a5b4b
Juan Pablo Garibotti Arias <jp.garibotti@gmail.com>
2026-03-17 18:31:36 +0100

    Added presets and launch scripts

---
+++ chat.ps1
@@ -0,0 +1,4 @@
+.\build\x64-cv-rel\bin\llama-cli.exe -ngl -1 -sysf "build\system-prompt.txt" -cnv -hf bartowski/deepseek-ai_DeepSeek-R1-0528-Qwen3-8B-GGUF:Q6_K_L
+#.\build\x64-cv-rel\bin\llama-cli.exe -ngl -1 -sysf "build\system-prompt.txt" -cnv -hf bartowski/mlabonne_Qwen3-8B-abliterated-GGUF:Q6_K_L
+#.\build\x64-cv-rel\bin\llama-cli.exe -ngl -1 -cnv -hf bartowski/mlabonne_Qwen3-8B-abliterated-GGUF:Q6_K_L
+#.\build\x64-cv-rel\bin\llama-cli.exe -ngl -1 -cnv -hf bartowski/deepseek-ai_DeepSeek-R1-0528-Qwen3-8B-GGUF:Q6_K_L
---
+++ web.ps1
@@ -0,0 +1,2 @@
+#.\build\x64-cv-rel\bin\llama-server.exe -dev CUDA0 -ngl -1 -hf mradermacher/Qwen3-VL-8B-Abliterated-Caption-it-GGUF:Q6_K
+.\build\x64-cv-rel\bin\llama-server.exe -dev CUDA0 -ngl -1 -hf unsloth/Qwen3-VL-8B-Instruct-GGUF:UD-Q6_K_XL
